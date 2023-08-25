b5ccc44552a1d154828a6f2a75a43961dbc0a5e1
danielswirsky <92892002+danielswirsky@users.noreply.github.com>
2023-08-24 20:36:28 -0500

    Finished Exploratory Data Analysis

--- housing_prediction.py
+++ housing_prediction.py
@@ -4,8 +4,41 @@ import numpy as np
 df = pd.read_csv(r"C:\Users\danie\OneDrive\Desktop\Programming\Learning ML\Housing Portfolio Proj\Housing.csv")
 #print(df.head())
 
+#EXPLORATORY DATA ANALYSIS
+
 #Check for Null values
 #print(df.info())
 
-print(df[df['area'] < (df.area.mean() + (-3*df.area.std()))])
-print(df[df.area > (df.area.mean() + (3*df.area.std()))])
+#print(df.shape)
+#print(df[df['area'] < (df.area.mean() + (-3*df.area.std()))])
+#print(df[df.area > (df.area.mean() + (3*df.area.std()))])
+df = df[df.area < (df.area.mean() + (3*df.area.std()))]
+#print(df.shape)
+
+#print(df.dtypes)
+print(df.furnishingstatus.unique())
+
+#Can manually replace object colums
+#or use pandas get_dummies for OHE
+df.mainroad.replace(
+    {'yes': 1, 'no': 0}, inplace=True
+)
+df.guestroom.replace(
+    {'yes': 1, 'no': 0},inplace= True 
+)
+df.basement.replace(
+    {'yes': 1, 'no': 0}, inplace=True
+)
+df.hotwaterheating.replace(
+    {'yes': 1, 'no': 0}, inplace=True
+)
+df.airconditioning.replace(
+    {'yes': 1, 'no': 0}, inplace=True 
+)
+df.prefarea.replace(
+    {'yes': 1, 'no': 0}, inplace=True
+)
+
+df = pd.get_dummies(df, drop_first=True)
+print(df.dtypes)
+print(df.shape)
